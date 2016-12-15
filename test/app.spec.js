599c83345f5d39e4a804af93414b97a15f739a3d
junglemove <thibaut.visticot@gmail.com>
2016-12-15 12:32:59 +0100

    Chai expect test init

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,3 +1,10 @@
 /**
  * Created by Administrateur on 15/12/2016.
  */
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+
+var expect = chai.expect();
+
+var app = require('../app'); // si on veut utiliser mongoose, descendre la connection dans app.js
--- test/model/contact.test.js
+++ test/model/contact.test.js
@@ -1,3 +1,18 @@
 /**
  * Created by Administrateur on 15/12/2016.
  */
+const assert = require('assert');
+const Contact = require('../../model/contact');
+
+describe('class Contact array', function () {
+    describe('method find',function () {
+        it('should return 2 contacts',function () {
+            Contact.find((err, contacts) => {
+                assert.equal(contacts.length, 2);
+            });
+        });
+        it('should not contain errors',function (done) {
+            Contact.find(done); // verify que la callback ne retourn pas d'erreurs
+        });
+    });
+});
